28e82422608e8f38fd5868eb06ff23f82c9d31e5
whatshu <1355992759@qq.com>
2020-12-03 17:55:56 +0800

    finally finished

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
-import time
+# socket connect
+import socket
+
+s = socket.socket()
+# s.connect(("192.168.31.84", 3000))
+# s.connect(("192.168.31.132", 3000))
+
+# gpio starts
 import RPi.GPIO as GPIO
+
+FREQUENCY = 120
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.cleanup()
 
-p1 = GPIO.PWM(18, 240)
-p2 = GPIO.PWM(17, 240)
-p1.start(0)
-p2.start(60)
-try:
-    while 1:
-        for dc in range(0, 60):
-            p1.ChangeDutyCycle(dc)
-            p2.ChangeDutyCycle(60 - dc)
-            time.sleep(0.01)
-        for dc in range(60, 0, -1):
-            p1.ChangeDutyCycle(dc)
-            p2.ChangeDutyCycle(60 - dc)
-            time.sleep(0.01)
-except KeyboardInterrupt:
-    pass
+left_forward = GPIO.PWM(18, FREQUENCY)
+left_backward = GPIO.PWM(17, FREQUENCY)
+right_forward = GPIO.PWM(22, FREQUENCY)
+right_backward = GPIO.PWM(23, FREQUENCY)
+left_forward.start(0)
+left_backward.start(0)
+right_forward.start(0)
+right_backward.start(0)
+
+while True:
+    left_forward.ChangeDutyCycle(100)
+    left_backward.ChangeDutyCycle(0)
+    right_backward.ChangeDutyCycle(100)
+    right_forward.ChangeDutyCycle(0)
+
+# while True:
+#     data = s.recv(1024)
+#     speed = (int(data[3]) - 100, int(data[4]) - 100)
+#     print(speed)
+#     if speed[0] < 0:
+#         left_backward.ChangeDutyCycle(-speed[0])
+#         left_forward.ChangeDutyCycle(0)
+#     else:
+#         left_forward.ChangeDutyCycle(speed[0])
+#         left_backward.ChangeDutyCycle(0)
+#
+#     if speed[1] < 0:
+#         right_backward.ChangeDutyCycle(-speed[1])
+#         right_forward.ChangeDutyCycle(0)
+#     else:
+#         right_forward.ChangeDutyCycle(speed[1])
+#         right_backward.ChangeDutyCycle(0)
